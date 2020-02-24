f8accc9b99c704f7b320e2eb387298b3d09effc3
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-02-24 17:16:37 +0100

    Add action for create events

---
+++ src/actions/events.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export const EVENT_CREATE_SUCCESS = "EVENT_CREATE_SUCCESS";
+
+function createEventSuccess(event) {
+  return {
+    type: EVENT_CREATE_SUCCESS,
+    payload: {
+      event: event
+    }
+  };
+}
+
+export const createEvent = (title, date, imageUrl) => {
+  return async function(dispatch, getState) {
+    const token = getState().user.token;
+
+    console.log(title, date, imageUrl);
+    dispatch({ type: "TESTING" });
+
+    const response = await axios({
+      method: "POST",
+      url: "http://localhost:4000/events",
+      headers: { authorization: `Bearer ${token}` },
+      data: {
+        title,
+        date,
+        imageUrl
+      }
+    });
+
+    console.log(response);
+    dispatch(createEventSuccess(response.data));
+  };
+};
