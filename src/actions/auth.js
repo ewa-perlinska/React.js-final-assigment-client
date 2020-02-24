eb5fcceedcc64477963ccf46cb3341f98e57b685
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-02-24 15:16:40 +0100

    Action for Login feature

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const USER_CREATED = "USER_CREATED";
 
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+
 const baseUrl = "http://localhost:4000";
 
 function signUpSuccess() {
@@ -19,6 +21,27 @@ export function signUp(username, email, password) {
       if (response.status === 201) {
         dispatch(signUpSuccess());
       }
+    } catch (error) {
+      throw error;
+    }
+  };
+}
+
+function loginSuccess(data) {
+  return {
+    type: LOGIN_SUCCESS,
+    payload: { data: data }
+  };
+}
+export function login(email, password) {
+  return async function(dispatch, getState) {
+    try {
+      const response = await axios.post(`${baseUrl}/login`, {
+        email: email,
+        password: password
+      });
+      // console.log("what is my response?", response.data.token);
+      dispatch(loginSuccess(response.data));
     } catch (error) {
       throw error;
     }
