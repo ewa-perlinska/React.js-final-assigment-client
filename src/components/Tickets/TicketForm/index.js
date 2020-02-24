5b6dedc729de5d1efb0a6db680223fcba96181be
Entliczekperliczek <ewaperlinska55@gmail.com>
2020-02-24 23:31:20 +0100

    Add Ticket Form and Ticket Form Container

---
+++ src/components/Tickets/TicketForm/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function TicketForm(props) {
+  const {
+    handleChange,
+    handleSubmit,
+    values: { image, price, description }
+  } = props;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Image</label>
+      <input onChange={handleChange} name="image" value={image} />
+      <label>Price</label>
+      <input onChange={handleChange} name="price" value={price} />
+      <label>Description</label>
+      <input onChange={handleChange} name="description" value={description} />
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default TicketForm;
